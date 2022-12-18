4e703cc741c8897bf02308e2f23891dc48496b66
Manuel Camacho <manuelcamacho0159@gmail.com>
2022-12-18 17:18:24 -0500

    got all the buttons to work without creating more, app works as intended only thing missing is CSS file.... I hate CSS

--- assests/script.js
+++ assests/script.js
@@ -1,41 +1,42 @@
 var searchFormEl = document.querySelector('#search-form');
 var fiveDay = document.querySelector('#five-day');
+var today = document.querySelector('#today');
+var search = document.querySelector("#search");
+var cityButton = document.querySelector('#cityButton');
 var lat = 0;
 var lon = 0;
-var tempTest =[];
+var aButton = [];
+if(localStorage.getItem('city')){
+    aButton = JSON.parse(localStorage.getItem('city'));
+}
 
 
 function handleSearchFormSubmit(event){
     event.preventDefault();
-
+    $('#search').empty();
+    $('#five-day').empty();
 
     var searchInputVal = document.querySelector('#search-input').value;
-
-    localStorage.setItem('city', searchInputVal);
-    
+    display(searchInputVal);
+    var result =testRepeat(searchInputVal);
+    if(result == 0 ){
+        aButton.push(searchInputVal);
+        localStorage.setItem('city', JSON.stringify(aButton));
+        buttonSetUp();
+    }
+    else{
+        buttonSetUp(); 
+    }
     if(!searchInputVal){
         return;
     }
 
-    var apiUrl
-    = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInputVal + "&limit=5&appid=a14ace2b2c6252a8f47e6628c07badaa";
- 
-    fetch(apiUrl)
-        .then(function(response){
-            if(response.ok){
-                response.json().then(function(data){;
-                    lat = data[0].lat;
-                    lon = data[0].lon;
-                    getWeather(lat, lon);
-            
-                });
-            }
-        })
-
-     
 }
 
 function getWeather(lat, lon){
+    $('#search').empty();
+    $('#five-day').empty();  
+    buttonSetUp();
     var x = 3;
     var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=a14ace2b2c6252a8f47e6628c07badaa&units=imperial";
     fetch(apiUrl)
@@ -43,7 +44,6 @@ function getWeather(lat, lon){
         if(response.ok){
             response.json().then(function(data){
                 for(var i =0; i < 5; i++){
-                    console.log(data);
                     var date = data.list[x].dt_txt.split(' ')[0];
                     var card = document.createElement('div');
                     card.classList.add('card');
@@ -70,5 +70,79 @@ function getWeather(lat, lon){
         }
     })
 }
+/*function getToday(lat, lon){
+    var apiUrl =
+    "https://api.openweathermap.org/data/3.0/onecall?lat="+lat+"&lon="+lon+"&appid=571cedbbc3e4d8b8a1ada9c2e332a9c8"
+    fetch(apiUrl)
+        .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+                    date = data.list.dt_txt;
+                    console.log(data);
+                    var time =document.createElement('p');
+                    time.innerHTML =
+                    '<strong>test</strong>'; 
+                })
+            }              
+        })
+ 
+}*/
+
+
+function buttonSetUp(){
+    if(aButton){
+        for(var i = 0; i < aButton.length; i++){
+            var button = document.createElement('button');
+            button.classList.add('btn')
+            button.innerHTML = aButton[i];
+            test = aButton[i];
+            button.onclick = (function(test){
+                return function(){
+                    $('#search').empty();
+                    $('#five-day').empty();
+                    display(test);
+                }
+            })(aButton[i])
+            search.append(button);
+        }
+    }
+}
+
+function display(searchInputVal){
+    var apiUrl
+    = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchInputVal + "&limit=5&appid=a14ace2b2c6252a8f47e6628c07badaa";
+    fetch(apiUrl)
+        .then(function(response){
+            if(response.ok){
+                response.json().then(function(data){
+
+                    lat = data[0].lat;
+                    lon = data[0].lon;
+                    getWeather(lat, lon);
+                   // getToday(lat, lon);
+                   localStorage.setItem(searchInputVal, searchInputVal);
+                });
+            }
+        })
+}
+function testRepeat(searchInputVal){
+    
+    if(aButton){
+        for(var i =0; i < aButton.length; i++){
+            if(searchInputVal == aButton[i]){
+                return 1;
+            }
+            if(searchInputVal == ""){
+                return 1;
+            }
+        }
+        return 0;   
+    }
+    return 0;
+}
+
+
+buttonSetUp();
+
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
